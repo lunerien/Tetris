e0d6f10cb36c139abd8ed3a074c6ee69246b011d
Lunerien <luki90003@gmail.com>
2019-08-24 17:29:17 +0200

    First Commit

---
+++ src/game/Game.java
@@ -0,0 +1,21 @@
+package game;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+
+
+    public synchronized void start(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("kot");
+    }
+}
